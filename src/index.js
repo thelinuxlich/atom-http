fdf84143c55b2c9d6409f3f86478d8efe4fe8d98
Alisson <thelinuxlich@gmail.com>
2021-10-05 10:57:59 -0300

    extract the boilerplate into a general helper

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import RedisStreamHelper from "redis-stream-helper"
+import { enrichStandardFields } from "atom-helper"
 
 const { listenForMessages, createStreamGroup, addStreamData, addListener } =
   RedisStreamHelper(process.env.REDIS_PORT, process.env.REDIS_HOST)
@@ -9,22 +10,8 @@ addListener("atom:http:trigger")
 
 const run = async () => {
   await listenForMessages(async (key, streamId, data) => {
-    data.generatedValue =
-      typeof data.generatedValue === "number" ? ++data.generatedValue : 1
+    enrichStandardFields("http", data, addStreamData)
     console.log("http executed with data", key, streamId, data)
-    await addStreamData("atom:http:complete", data)
-    // treating only WS for now
-    if (data.transport && data.transport === "ws") {
-      data.origin = "atom:http:trigger"
-      addStreamData("transport:ws:trigger", data)
-    }
-    if (data.execution && data.process) {
-      addStreamData("execution:trigger", {
-        process: data.process,
-        id: data.execution,
-        payload: data.payload + " - " + data.generatedValue,
-      })
-    }
   })
   run()
 }
